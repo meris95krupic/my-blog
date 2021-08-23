0cc3ede9c7da627e2f19b61177f6c7d9ed5f6f84
Meris <meris-k@hotmail.com>
2021-08-23 22:30:00 +0200

    3. Migrationsschritt (e2e mit cypress) -> DONE

---
+++ client/cypress/integration/auth.js
@@ -0,0 +1,32 @@
+describe("login and register", () => {
+    it("login should fail", () => {
+        cy.visit("/login")
+        cy.get('[type="text"]').clear();
+        cy.get('[type="text"]').type('test');
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('testtest');
+        cy.get('.loginButton').click();
+        cy.get(".home").should("not.exist")
+    })
+
+    it("login correctly", () => {
+        cy.visit("/login")
+        cy.get('[type="text"]').clear();
+        cy.get('[type="text"]').type('test');
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('test');
+        cy.get('.loginButton').click();
+        cy.get(".home").should("exist")
+    })
+
+    it("register should fail", () => {
+        cy.visit("/register")
+        cy.get('[placeholder="Enter your username..."]').clear();
+        cy.get('[placeholder="Enter your username..."]').type('test222');
+        cy.get('[placeholder="Enter your email..."]').clear();
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('test222');
+        cy.get('.registerButton').click();
+        cy.contains("Something went wrong!")
+    })
+})
---
+++ client/cypress/integration/posts.js
@@ -0,0 +1,74 @@
+describe("posts: write, update and delete", () => {
+    it("write post correctly", () => {
+        cy.visit("/login")
+        // Login
+        cy.get('[type="text"]').clear();
+        cy.get('[type="text"]').type('test');
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('test');
+        cy.get('.loginButton').click();
+        // write a post
+        cy.get(':nth-child(4) > .link').click();
+        cy.get(':nth-child(1) > .writeInput').clear();
+        cy.get(':nth-child(1) > .writeInput').type('test-abc');
+        cy.get(':nth-child(2) > .writeInput').clear();
+        cy.get(':nth-child(2) > .writeInput').type('Test---abc...');
+        cy.get('.writeSubmit').click();
+        
+        cy.contains("Author:")
+        cy.contains("test-abc")
+    })
+
+    it("writing a post should fail", () => {
+        cy.visit("/login")
+        // Login
+        cy.get('[type="text"]').clear();
+        cy.get('[type="text"]').type('test');
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('test');
+        cy.get('.loginButton').click();
+        // write a post that fails
+        cy.get(':nth-child(4) > .link').click();
+        cy.get(':nth-child(1) > .writeInput').clear();
+        cy.get(':nth-child(1) > .writeInput').type('ABC');
+        cy.get('.writeSubmit').click();
+        
+        cy.contains("Author:").should("not.exist")
+        cy.contains("ABC").should("not.exist")
+    })
+
+    it("update post correctly", () => {
+        cy.visit("/login")
+        // Login
+        cy.get('[type="text"]').clear();
+        cy.get('[type="text"]').type('test');
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('test');
+        cy.get('.loginButton').click();
+        // update a post
+        cy.get('.postTitle').contains('test-abc').click();
+        cy.get('.fa-edit').click();
+        
+        cy.get('.singlePostDescInput').clear();
+        cy.get('.singlePostDescInput').type('Test---abc...123---abc');
+        cy.get('.singlePostButton').click();
+
+        cy.get('.topList > :nth-child(1) > .link').click();
+        cy.contains("Test---abc...123---abc").should("exist")
+    })
+
+    it("delete post correctly", () => {
+        cy.visit("/login")
+        // Login
+        cy.get('[type="text"]').clear();
+        cy.get('[type="text"]').type('test');
+        cy.get('[type="password"]').clear();
+        cy.get('[type="password"]').type('test');
+        cy.get('.loginButton').click();
+        // delete a post
+        cy.get('.postTitle').contains('test-abc').click();
+        cy.get('.fa-trash-alt').click();
+        
+        cy.contains("test-abc").should("not.exist")
+    })
+})
